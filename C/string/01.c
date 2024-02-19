3b28fd769138992c9f3b7de3e6dd5c7f7c41e780
Seom <112147135+seom-j@users.noreply.github.com>
2024-02-19 15:24:05 +0900

    Delete C directory

--- C/string/01.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-#pragma warning(disable:4996)
-
-int main() {
-	char str[20] = { NULL };
-	scanf("%s", str);
-	for (int i = 0; str[i] != 0; i++) if (str[i] >= 'a' && str[i] <= 'z') printf("%c", str[i]);
-}
