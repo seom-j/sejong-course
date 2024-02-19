b9aa3eebfe80d90823242e1e9e6c87ae38bb57dd
Seom <112147135+seom-j@users.noreply.github.com>
2024-02-19 23:55:25 +0900

    Create table-init.sql

---
+++ database/the-best-accommodation-finder/table-init.sql
@@ -0,0 +1,11 @@
+-- 외래키 설정
+ALTER TABLE detail_info
+ADD FOREIGN kEY detail_info(Aid)
+REFERENCES basic_info(Aid);
+ALTER TABLE review_info
+ADD FOREIGN kEY review_info(Aid)
+REFERENCES basic_info(Aid);
+
+-- location_info, type_info table 생성
+CREATE TABLE location_info AS SELECT DISTINCT location FROM basic_info;
+CREATE TABLE type_info AS SELECT DISTINCT Type FROM basic_info;
